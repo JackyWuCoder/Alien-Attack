d6c6a9c365f5e40716ceaed5279fca78083d03df
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-01 15:56:25 -0800

    Created a speed variable

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
+var speed = 400
+
 func _physics_process(delta):
 	velocity = Vector2(0, 0)
 	if Input.is_action_pressed("move_right"):
-		velocity.x = 100
+		velocity.x = speed
 	if Input.is_action_pressed("move_left"):
-		velocity.x = -100
+		velocity.x = -speed
 	if Input.is_action_pressed("move_up"):
-		velocity.y = -100
+		velocity.y = -speed
 	if Input.is_action_pressed("move_down"):
-		velocity.y = 100	
+		velocity.y = speed	
 	move_and_slide()
