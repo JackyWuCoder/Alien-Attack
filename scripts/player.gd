fbaa8f2bd4832b79b64c8a1db08d20143f6ce21a
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-01 15:55:01 -0800

    Using Input Action Map for Player WASD movements

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+func _physics_process(delta):
+	velocity = Vector2(0, 0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x = 100
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -100
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -100
+	if Input.is_action_pressed("move_down"):
+		velocity.y = 100	
+	move_and_slide()
