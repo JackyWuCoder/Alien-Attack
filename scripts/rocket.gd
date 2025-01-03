b46f190035b7b025b2629b9259f9f85668b0f0a4
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-02 23:22:01 -0800

    Connecting a node signal to a script using in-script referencing
    1) .connect takes in the signal of the reference node that we want and whenever that signal is emitted it will call the _on_screen_exited callback function.

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -4,5 +4,11 @@ extends Area2D
 
 @onready var visible_notifier = $VisibleNotifier
 
+func _ready():
+	visible_notifier.connect("screen_exited", _on_screen_exited)
+
 func _physics_process(delta):
 	global_position.x += speed * delta
+
+func _on_screen_exited():
+	pass
