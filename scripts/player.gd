bd5dd2922c019e95c036c065bdd56cd6a4742645
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-02 22:50:09 -0800

    using preload instead of load
    1) load will attempt to load the file path every time it is called during runtime.
    2) preload will load the file path only once before the game actually starts.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var speed = 300
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -26,6 +28,5 @@ func _physics_process(delta):
 	global_position = global_position.clamp(Vector2(0, 0), screen_size)
 
 func shoot():
-	var rocket_scene = load("res://scenes/rocket.tscn")
 	var rocket_instance = rocket_scene.instantiate()
 	add_child(rocket_instance)
