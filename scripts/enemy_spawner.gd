fcf2775bd12421cc4b12df7018bd1a727ebcc11f
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-05 16:12:10 -0800

    Spawn enemies at random predefined positions.
    1) .get_children() returns an array of the current node's childrens. (spawn_positions_array)
    2) .pick_random() returns a random element of an array. (random_spawn_position)
    3) .global_position returns the global coordinates of the retrieved random position element.

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 var enemy_scene = preload("res://scenes/enemy.tscn")
 
 @onready var spawn_timer = $Timer;
+@onready var spawn_positions = $SpawnPositions
 
 func _ready():
 	spawn_timer.connect("timeout", _on_timer_timeout)
@@ -11,6 +12,9 @@ func _on_timer_timeout():
 	spawn_enemy()
 
 func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
 	var enemy_instance = enemy_scene.instantiate()
-	enemy_instance.global_position = Vector2(1350, 300)
+	enemy_instance.global_position = random_spawn_position.global_position
 	add_child(enemy_instance)
