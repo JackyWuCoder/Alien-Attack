3db838a3f77be8794d936dd3fadce82518457781
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-05 16:01:58 -0800

    Instantiate enemy to a predefined location

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
 @onready var spawn_timer = $Timer;
 
 func _ready():
@@ -7,3 +9,8 @@ func _ready():
 
 func _on_timer_timeout():
 	print("Timeout")
+
+func spawn_enemy():
+	var enemy_instance = enemy_scene.instantate()
+	enemy_instance.global_position = Vector2(1350, 300)
+	add_child(enemy_instance)
